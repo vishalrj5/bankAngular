68b3b11c9f99d5b7a6e50dc2280939cfcf8823c9
vishalrj5 <vishalrj5@gmail.com>
2021-07-04 23:06:05 +0530

    authentication

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,11 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  Aim= "Welcome to Luminar"
+  Aim = "Welcome to Luminar"
   acc = "Account number here"
-  accno=""
-  pwd=""
-  users = {
+  accno = ""
+  pwd = ""
+  users: any = {
     1000: { acno: 1000, username: "userone", password: "userone", actype: "savings", balance: 5000 },
     1001: { acno: 1001, username: "usertwo", password: "usertwo", actype: "savings", balance: 6000 },
     1002: { acno: 1002, username: "userthree", password: "userthree", actype: "savings", balance: 7000 },
@@ -21,17 +21,31 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
-accChange(event:any){
-  this.accno = event.target.value
-  
-  console.log(this.accno)
-}
-pwdchange(event:any){
-  this.pwd = event.target.value
-  console.log(this.pwd)
-}
+  accChange(event: any) {
+    this.accno = event.target.value
 
-  login(){
-    alert("Login Successful")
+    console.log(this.accno)
+  }
+  pwdchange(event: any) {
+    this.pwd = event.target.value
+    console.log(this.pwd)
+  }
+
+  login() {
+    var acno = this.accno
+    var pwd = this.pwd
+    let accDetails = this.users
+    if (acno in accDetails) {
+      if (pwd == accDetails[acno]["password"]) {
+        alert("Login Successful")
+      }
+      else{
+        alert("password invalid")
+      }
+    }
+    else {
+      alert("invalid credentials")
+    }
+
   }
 }
