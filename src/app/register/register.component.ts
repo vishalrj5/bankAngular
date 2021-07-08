7f14911e72b561990611f85cf689358910bf18d2
vishalrj5 <vishalrj5@gmail.com>
2021-07-08 10:34:45 +0530

    Reactive Forms

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -9,29 +10,43 @@ import { DataService } from '../services/data.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  acctype=""
-  accno=""
-  uname=""
-  pwd=""
+  acctype = ""
+  accno = ""
+  uname = ""
+  pwd = ""
 
-  constructor(private ds:DataService, private router:Router) { }
+  registerForm = this.fb.group({
+    uname: [''],
+    accno: ['', Validators.required, Validators.minLength(4), Validators.pattern('[0-9]*')],
+    acctype: ['', [Validators.required, Validators.pattern('[a-zA-Z]*')]],
+    pswd: ['']
+  })
+  constructor(private ds: DataService, private router: Router, private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
-  register(){
-    var acno = this.accno;
-    var uname = this.uname;
-    var pwd = this.pwd;
-    var actype = this.acctype
+  register() {
 
-    var result = this.ds.register(acno,uname,pwd,actype)
-    if(result){
-      alert("success")
-      this.router.navigateByUrl("")
+    var uname = this.registerForm.value.uname;
+    var acno = this.registerForm.value.accno;
+    var actype = this.registerForm.value.acctype;
+    var pswd = this.registerForm.value.pswd;
+
+    console.log(acno, uname, pswd, actype)
+    if (this.registerForm.valid) {
+      var result = this.ds.register(acno, uname, pswd, actype)
+      if (result) {
+        alert("success")
+        this.router.navigateByUrl("")
+      }
+      else {
+        alert("User already exist plaease login")
+        this.router.navigateByUrl("")
+      }
     }
-    else{
-      alert("User already exist plaease login")
-      this.router.navigateByUrl("")
+    else {
+      alert("Invlaid Form")
     }
   }
+
 }
